eb0f96c7996017c55f2337bcf49f497f5560a4b2
UmairQurashi <umairqurashi56@gmail.com>
2022-08-15 13:12:25 -0700

    add mongo Atlas

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const DB = "mongodb://127.0.0.1:27017/InventorySystem";
+
 const app = require("./app");
 require("dotenv").config({ path: `${__dirname}/../.env` });
 
 mongoose
-  .connect(DB, {
+  .connect(process.env.DBlink, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
